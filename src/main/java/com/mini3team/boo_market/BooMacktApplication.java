7371a65ce7c2e53619fcdcaae824b5cde7836a94
sunmin416 <rhtjsals0106@gmail.com>
2026-05-14 22:53:31 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/mini3team/boo_market/BooMacktApplication.java
@@ -0,0 +1,13 @@
+package com.mini3team.boo_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooMacktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooMacktApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mini3team/boo_market/BooMacktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini3team.boo_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooMacktApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
