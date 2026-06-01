7cbe04d5f7d0b62e7ff74623f0df65f5c32a1bc9
sunmin416 <rhtjsals0106@gmail.com>
2026-06-01 12:41:33 +0900

    [ADD] 본인 글 wish 금지

--- src/main/java/com/mini3team/boo_market/domain/wish/WishService.java
+++ src/main/java/com/mini3team/boo_market/domain/wish/WishService.java
@@ -1,9 +1,12 @@
 package com.mini3team.boo_market.domain.wish;
 
+import com.mini3team.boo_market.common.exception.ApiException;
 import com.mini3team.boo_market.domain.post.Post;
 import com.mini3team.boo_market.domain.post.PostRepository;
+import com.mini3team.boo_market.domain.user.UserRepository;
 import com.mini3team.boo_market.dto.response.WishListResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,16 +19,21 @@ public class WishService {
 
     private final WishRepository wishRepository;
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
-    public void addWish(Long postId, Long userId) {
+    public void addWish(Long postId, Long buyerId) {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
 
-        if (wishRepository.existsByUserIdAndPostId(userId, postId)) {
+        if (buyerId.equals(post.getAuthorId())) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "chat", "본인 게시글에는 채팅할 수 없습니다.");
+        }
+
+        if (wishRepository.existsByUserIdAndPostId(buyerId, postId)) {
             throw new IllegalArgumentException("이미 관심상품으로 등록된 게시글입니다.");
         }
 
-        wishRepository.save(Wish.builder().userId(userId).post(post).build());
+        wishRepository.save(Wish.builder().userId(buyerId).post(post).build());
     }
 
     public void removeWish(Long postId, Long userId) {
