7941439fe3eebba87bf28de55da8c9078dc3472c
sunmin416 <rhtjsals0106@gmail.com>
2026-06-01 12:45:29 +0900

    [FIX] WishService 버그 수정

--- src/main/java/com/mini3team/boo_market/domain/wish/WishService.java
+++ src/main/java/com/mini3team/boo_market/domain/wish/WishService.java
@@ -3,7 +3,6 @@ package com.mini3team.boo_market.domain.wish;
 import com.mini3team.boo_market.common.exception.ApiException;
 import com.mini3team.boo_market.domain.post.Post;
 import com.mini3team.boo_market.domain.post.PostRepository;
-import com.mini3team.boo_market.domain.user.UserRepository;
 import com.mini3team.boo_market.dto.response.WishListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,26 +18,25 @@ public class WishService {
 
     private final WishRepository wishRepository;
     private final PostRepository postRepository;
-    private final UserRepository userRepository;
 
-    public void addWish(Long postId, Long buyerId) {
+    public void addWish(Long postId, Long userId) {
         Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+                .orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND, "post", "존재하지 않는 게시글입니다."));
 
-        if (buyerId.equals(post.getAuthorId())) {
-            throw new ApiException(HttpStatus.BAD_REQUEST, "chat", "본인 게시글에는 채팅할 수 없습니다.");
+        if (userId.equals(post.getAuthorId())) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "wish", "본인 게시글에는 찜할 수 없습니다.");
         }
 
-        if (wishRepository.existsByUserIdAndPostId(buyerId, postId)) {
-            throw new IllegalArgumentException("이미 관심상품으로 등록된 게시글입니다.");
+        if (wishRepository.existsByUserIdAndPostId(userId, postId)) {
+            throw new ApiException(HttpStatus.CONFLICT, "wish", "이미 관심상품으로 등록된 게시글입니다.");
         }
 
-        wishRepository.save(Wish.builder().userId(buyerId).post(post).build());
+        wishRepository.save(Wish.builder().userId(userId).post(post).build());
     }
 
     public void removeWish(Long postId, Long userId) {
         Wish wish = wishRepository.findByUserIdAndPostId(userId, postId)
-                .orElseThrow(() -> new IllegalArgumentException("관심상품으로 등록되지 않은 게시글입니다."));
+                .orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND, "wish", "관심상품으로 등록되지 않은 게시글입니다."));
         wishRepository.delete(wish);
     }
 
